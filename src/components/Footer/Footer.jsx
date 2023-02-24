05f0a2f1c0a8ad6c5100fd0483f120c61261f3e3
Andrew Chernoff <whitedrew538@gmail.com>
2023-02-24 13:51:26 +0500

    1st part

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import styleContainer from "../../common/styles/ContainerStyles.module.css";
 
 const Footer = () => {
     return <div className={style.footer}>
-        <div className={`${styleContainer.container}`}>
-            <h2>Andrew Chernov</h2>
+        <div className={`${styleContainer.container} ${style.footerContainer}`}>
+            <h2 className={style.title}>Andrew Chernov</h2>
 
             <div className={style.footerContent}>
                 <div></div>
--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -10,9 +10,7 @@ const MyProjects = () => {
 
     <div className={style.projectItems}>
         <div className={style.projectItem}>
-          <div>
-            <img className={style.img} src="https://www.lenovo.com/medias/lenovo-laptops-thinkbook-16-gen-4-intel-hero.png?context=bWFzdGVyfHJvb3R8MzQ1OTM2fGltYWdlL3BuZ3xoMjEvaGZkLzEzMjU1MTI1OTkxNDU0LnBuZ3xlMGJjMDAyZjIzYzczYmY0YTY3NTlmODcwMDJjZTBhMzg5M2VlMjFlNTNlZWJkZDMyNDA3MTdlNjc3NjhhZWY5" alt="" />
-
+          <div className={style.imageContainer}>
             <button>Look</button>
           </div>
 
@@ -25,9 +23,7 @@ const MyProjects = () => {
         </div>
 
         <div className={style.projectItem}>
-          <div>
-            <img className={style.img} src="https://www.lenovo.com/medias/lenovo-laptops-thinkbook-16-gen-4-intel-hero.png?context=bWFzdGVyfHJvb3R8MzQ1OTM2fGltYWdlL3BuZ3xoMjEvaGZkLzEzMjU1MTI1OTkxNDU0LnBuZ3xlMGJjMDAyZjIzYzczYmY0YTY3NTlmODcwMDJjZTBhMzg5M2VlMjFlNTNlZWJkZDMyNDA3MTdlNjc3NjhhZWY5" alt="" />
-
+          <div className={style.imageContainer}>
             <button>Look</button>
           </div>
           
