f952ec0b853069af4a1cc10d3cf4cf1142f84725
Andrew Chernoff <whitedrew538@gmail.com>
2023-05-31 00:08:55 +0500

    styling is probably finished

--- src/components/Contact/ContactForm/ContactForm.jsx
+++ src/components/Contact/ContactForm/ContactForm.jsx
@@ -12,7 +12,11 @@ import style from './ContactForm.module.scss'
      .min(2, 'Too Short!')
      .max(50, 'Too Long!')
      .required('Required'),
-  // email: Yup.string().email('Invalid email').required('Required'),
+   email: Yup.string().email('Invalid email').required('Required'),
+   help: Yup.string()
+     .min(1, 'Too Short!')
+     .max(50, 'Too Long!')
+     .required('Required'),
  });
  
 const ContactFrom = () => {
@@ -69,16 +73,19 @@ const ContactFrom = () => {
           <div className={style.field__wrapper}>
            <label htmlFor='name'>What is Your Name:</label>
            <Field type="text" name="name" autocomplete="off" className={style.textarea__field}/>
+           {errors.name && touched.name ? <div>{errors.name}</div> : null}
           </div>
 
           <div className={style.field__wrapper}>
            <label htmlFor='email'>Your Email Address:</label>
            <Field type="text" name="email" autocomplete="off" className={style.textarea__field}/>
+           {errors.email && touched.email ? <div>{errors.email}</div> : null}
           </div>
 
           <div className={style.field__wrapper}>
            <label htmlFor='help'>How can I Help you?:</label>
            <Field type="text" name="help" as='textarea' autocomplete="off" className={style.textarea__field}/>
+           {errors.help && touched.help ? <div>{errors.help}</div> : null}
           </div>
            
            {/* <div>
