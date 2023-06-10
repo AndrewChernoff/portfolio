568660046447b2e63334ffa352317f0a207a0ef6
Andrew Chernoff <whitedrew538@gmail.com>
2023-06-10 15:53:46 +0500

    typing effect

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,3 +1,4 @@
+import ReactTypingEffect from 'react-typing-effect';
 import style from './Main.module.scss';
 import styleContainer from '../../common/styles/ContainerStyles.module.css';
 
@@ -6,7 +7,28 @@ const Main = () => {
         <div className={styleContainer.container}>
             <div className={style.text}>
                 <span>HI, I'M FRONTEND DEVELOPER</span>
-                <h1>Andrew</h1>
+                {/* <h1>Andrew</h1> */}
+                <ReactTypingEffect
+                style={{maxHeight: '233px'}}
+        text={["Andrew"]}
+        cursorRenderer={cursor => <h1>{cursor}</h1>}
+        displayTextRenderer={(text, i) => {
+          return (
+            <h1 className={style.name}>
+              {text.split('').map((char, i) => {
+                const key = `${i}`;
+                return (
+                  <h1
+                    key={key}
+                    style={i%2 === 0 ? { color: 'white'} : {}}
+                  >{char}</h1>
+                );
+              })}
+            </h1>
+          );
+        }}        
+      />
+
                 <p>based in Chelyabinsk, Russia</p>
                 
                 <a href='#myWorks'>View my works</a>
