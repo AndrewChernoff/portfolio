5a50d2d1ad69f566bb81e303bb1a5d215d7581b1
Andrew Chernoff <whitedrew538@gmail.com>
2024-04-29 15:01:34 +0500

    changed projects section and typing effect

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,41 +1,45 @@
-import { Link } from 'react-scroll'
-import ReactTypingEffect from 'react-typing-effect';
-import style from './Main.module.scss';
-import styleContainer from '../../common/styles/ContainerStyles.module.css';
+import { Link } from "react-scroll";
+import ReactTypingEffect from "react-typing-effect";
+import style from "./Main.module.scss";
+import styleContainer from "../../common/styles/ContainerStyles.module.css";
 
 const Main = () => {
-    return <div className={style.main} id='main'>
-        <div className={styleContainer.container}>
-            <div className={style.text}>
-                <span>HI, I'M FRONTEND DEVELOPER</span>
-                {/* <h1>Andrew</h1> */}
-                <ReactTypingEffect
-                style={{maxHeight: '233px'}}
-        text={["Andrew"]}
-        cursorRenderer={cursor => <h1>{cursor}</h1>}
-        displayTextRenderer={(text, i) => {
-          return (
-            <h1 className={style.name}>
-              {text.split('').map((char, i) => {
-                const key = `${i}`;
-                return (
-                  <h1
-                    key={key}
-                    style={i%2 === 0 ? { color: 'white'} : {}}
-                  >{char}</h1>
-                );
-              })}
-            </h1>
-          );
-        }}        
-      />
+  return (
+    <div className={style.main} id="main">
+      <div className={styleContainer.container}>
+        <div className={style.text}>
+          <span>HI, I'M FRONTEND DEVELOPER</span>
+          <ReactTypingEffect
+            text={["Andrew"]}
+            cursorRenderer={(cursor) => <h1>{cursor}</h1>}
+            displayTextRenderer={(text, i) => {
+              return (
+                <h1 className={style.smt}>
+                  {text.split("").map((char, i) => {
+                    const key = `${i}`;
+                    return <span key={key}>{char}</span>;
+                  })}
+                </h1>
+              );
+            }}
+          />
 
-                <p>based in Chelyabinsk, Russia</p>
-                
-                <Link activeClass={style.active} to="myWorks" className={style.worksLink} spy={true} smooth={true} duration={500}>View my works</Link>
-            </div>
-            </div>
+          <p>based in Chelyabinsk, Russia</p>
+
+          <Link
+            activeClass={style.active}
+            to="myWorks"
+            className={style.worksLink}
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            View my works
+          </Link>
         </div>
-}
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -2,62 +2,93 @@ import style from "./MyProjects.module.scss";
 import styleContainer from "../../common/styles/ContainerStyles.module.css";
 import Title from "../../common/components/Title";
 import Project from "./Project";
-import socialNetwork from "../../assets/images/social_network.jpg"
-import todolist from "../../assets/images/todolist.png"
-import carLeasing from "../../assets/images/carCalc.jpg"
-import quiz from "../../assets/images/quiz.jpg"
-import counter from "../../assets/images/counter.png"
-import lebron from "../../assets/images/lebron.jpg"
+import todolist from "../../assets/images/todolist.png";
+import carLeasing from "../../assets/images/carCalc.jpg";
+import quiz from "../../assets/images/quiz.jpg";
+import counter from "../../assets/images/counter.png";
+import lebron from "../../assets/images/lebron.jpg";
+import flashcards from "../../assets/images/flashcards.webp";
 
 const MyProjects = () => {
-
-  const socialNetworkImg = {
-    background: `url(${socialNetwork}) center center/cover no-repeat`
-  }
+  const flashcardsImg = {
+    background: `url(${flashcards}) center center/cover no-repeat`,
+  };
 
   const todolistImg = {
-    background: `url(${todolist}) center center/cover no-repeat`
-  }
+    background: `url(${todolist}) center center/cover no-repeat`,
+  };
 
   const carLeasingImg = {
-    background: `url(${carLeasing}) center center/cover no-repeat`
-  }
+    background: `url(${carLeasing}) center center/cover no-repeat`,
+  };
 
   const quizImg = {
-    background: `url(${quiz}) center center/cover no-repeat`
-  }
+    background: `url(${quiz}) center center/cover no-repeat`,
+  };
 
   const counterImg = {
-    background: `url(${counter}) center center/cover no-repeat`
-  }
+    background: `url(${counter}) center center/cover no-repeat`,
+  };
 
   const lebronImg = {
-    background: `url(${lebron}) center center/cover no-repeat`
-  }
-  
+    background: `url(${lebron}) center center/cover no-repeat`,
+  };
 
   const projects = [
-    {title: 'Social Network', backgroundStyle: socialNetworkImg, link: 'https://react-network.vercel.app/#/login'},
-    {title: 'Todolist', backgroundStyle: todolistImg, link: 'https://todolist-redux-toolkit-pi.vercel.app/login'},
-    {title: 'Car leasing calculator', backgroundStyle: carLeasingImg, link: 'https://andrewchernoff.github.io/carleasing'},
-    {title: 'Quiz', backgroundStyle: quizImg, link: 'https://andrewchernoff.github.io/quiz'},
-    {title: 'Counter', backgroundStyle: counterImg, link: 'https://counter-gbz8.vercel.app/'},
-    {title: 'NBA players', backgroundStyle: lebronImg, link: 'https://players-trading.vercel.app/'},
-  ]
+    {
+      title: "Todolist",
+      backgroundStyle: todolistImg,
+      link: "https://todolist-redux-toolkit-pi.vercel.app/login",
+    },
+    {
+      title: "Car leasing calculator",
+      backgroundStyle: carLeasingImg,
+      link: "https://andrewchernoff.github.io/carleasing",
+    },
+    {
+      title: "Quiz",
+      backgroundStyle: quizImg,
+      link: "https://andrewchernoff.github.io/quiz",
+    },
+    {
+      title: "Counter",
+      backgroundStyle: counterImg,
+      link: "https://counter-gbz8.vercel.app/",
+    },
+    {
+      title: "NBA players",
+      backgroundStyle: lebronImg,
+      link: "https://players-trading.vercel.app/",
+    },
+    {
+      title: "Flashcards",
+      backgroundStyle: flashcardsImg,
+      link: "https://flashcards-3d8nivnys-andrewchernoff.vercel.app/",
+    },
+  ];
 
   return (
-    <div className={style.projectsBlock} id='myWorks'>
+    <div className={style.projectsBlock} id="myWorks">
       <div className={`${styleContainer.container} ${style.projectsContainer}`}>
+        <Title
+          title={"My Projects"}
+          titleDescr={"Some of my recent projects"}
+        />
 
-    <Title title={'My Projects'} titleDescr={'Some of my recent projects'}/>
-
-    <div className={style.projectItems}>
-{/*         <Project backgroundStyle={socialNetworkImg} title={'Social Network'} link={'https://react-network.vercel.app/#/login'} />
+        <div className={style.projectItems}>
+          {/*         <Project backgroundStyle={socialNetworkImg} title={'Social Network'} link={'https://react-network.vercel.app/#/login'} />
         <Project backgroundStyle={todolistImg} title={'Todolist'} link={'https://todolist-redux-toolkit-pi.vercel.app/login'} /> */}
-        {projects.map((p, i) => {
-          return <Project key={i} backgroundStyle={p.backgroundStyle} title={p.title} link={p.link}/>
-        })}
-    </div>
+          {projects.map((p, i) => {
+            return (
+              <Project
+                key={i}
+                backgroundStyle={p.backgroundStyle}
+                title={p.title}
+                link={p.link}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
